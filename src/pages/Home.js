0aca4e4f9cc717c8bb8cdb1b25addecb9e45aa5d
noveler-study <103440394+noveler-study@users.noreply.github.com>
2024-10-21 00:24:03 +0900

    fcm 기능 반영

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -141,17 +141,278 @@ function Home(props) {
 function NoDiary(props) {
     return (
         <Container>
-            {/* 여기에 NoDiary 화면 구현 */}
+            <Row>
+                <div className="loading_box_home_top">
+
+                    <span className="desktop-view">
+                        <b>안녕하세요</b> 😀<br/>Pocket Mind에 오신걸 환영합니다.
+            </span>
+                    <span className="smartphone-view">
+                        <b>안녕하세요</b> 😀<br/> 환영해요
+            </span>
+
+
+                </div>
+            </Row>
+            <Row>
+                <div className="loading_box_home_bottom">
+
+                    <span className="desktop-view">
+                        <div>🥲 아직 작성한 일기가 없어요. 첫 일기를 작성해볼까요?
+                        </div>
+                        &nbsp;
+                        <div><Button
+                            variant="primary"
+                            style={{backgroundColor: "007AFF", fontWeight: "600"}}
+                            onClick={props.navigateToWriting}>
+                            📝 오늘의 일기 작성하러 가기
+                        </Button>
+                       </div>
+                    </span>
+                    <span className="smartphone-view-text">
+                        🥲 아직 작성한 일기가 없어요.<br/>첫 일기를 작성해볼까요?
+                    <div className="d-grid gap-2">
+                            &nbsp;
+                        <Button
+                            variant="primary"
+                            style={{backgroundColor: "007AFF", fontWeight: "600"}}
+                            onClick={props.navigateToWriting}>
+                            📝 오늘의 일기 작성하러 가기
+                        </Button>
+
+                        </div>
+
+                    </span>
+
+
+                </div>
+                {/*<Row>
+                    <Col>
+                        <div className="d-grid gap-2">
+                            <Button
+                                variant="primary"
+                                style={{fontWeight: "600"}}
+                                onClick={props.navigateToWriting}
+                            >✏️ 일기 작성하기</Button>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="d-grid gap-2">
+                            <Button
+                                variant="primary"
+                                style={{fontWeight: "600"}}
+                                onClick={props.navigateToReview}
+                            >📖 일기 돌아보기</Button>
+                        </div>
+
+                    </Col>
+                </Row>*/}
+                <span className="center_temp">
+                                                &nbsp;
+
+                    <Row xs={1} md={2} className="g-4">
+
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={book_purple}/>
+                            <Card.Body>
+                                <Card.Title><b>일기쓰기와 정신건강</b></Card.Title>
+                                <Card.Text>
+                                    일기를 작성하는 것이 어떻게 정신건강에 도움이 될까요?
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={chat}/>
+                            <Card.Body>
+                                <Card.Title><b>누구와 말하는 건가요?</b></Card.Title>
+                                <Card.Text>
+                                    마음챙김 다이어리가 어떻게 동작 원리에 대해 알아봅니다.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={lock}/>
+                            <Card.Body>
+                                <Card.Title><b>개인정보는 어떻게 관리되나요?</b></Card.Title>
+                                <Card.Text>
+                                    나의 데이터는 어떻게 관리되는지 알아봅니다.</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={book_blue}/>
+                            <Card.Body>
+                                <Card.Title><b>어떻게 적는건가요?</b></Card.Title>
+                                <Card.Text>
+                                    정신건강에 도움이 되는 일상 기록이란?
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+                </span>
+            </Row>
+            <div className="footer"></div>
         </Container>
-    );
+    )
 }
 
 function Loading_complete(props) {
     return (
         <Container>
-            {/* 여기에 Loading_complete 화면 구현 */}
+            <Row>
+                <div className="loading_box_home_top">
+
+                    <span className="desktop-view">
+                        <b>안녕하세요</b> 😀<br/>마음챙김 다이어리에 오신걸 환영합니다.
+            </span>
+                    <span className="smartphone-view">
+                        <b>안녕하세요</b> 😀<br/> 환영해요
+            </span>
+
+
+                </div>
+            </Row>
+            <Row>
+                <div className="loading_box_home_bottom">
+
+                    <span className="desktop-view">
+<div>
+                        📅 마지막으로 작성한 일기는 <b>{props.Unix_timestamp(props.lastDate)}</b> 일기에요.
+                        <br/>
+                        📖 지금까지 <b>{props.diaryList.length}</b>개의 일기를 작성하셨네요!
+                    </div>
+                        &nbsp;
+                        <div>
+
+                            <Button
+                                variant="primary"
+                                style={{backgroundColor: "007AFF", fontWeight: "600"}}
+                                onClick={props.navigateToWriting}>
+                            📝 오늘의 일기 작성하러 가기
+                        </Button>
+                        &nbsp;&nbsp;
+                            <Button
+                            variant="dark"
+                            style={{backgroundColor: "6c757d", fontWeight: "600"}}
+                            onClick={props.navigateToReview}>
+                            📖 작성한 일기 다시보기
+                        </Button>
+
+
+                       </div>
+                    </span>
+                    <span className="smartphone-view-text">
+<div>
+                        📅 마지막 일기는 <b>{props.Unix_timestamp(props.lastDate)}</b> 일기에요.
+                        <br/>
+                        📖 지금까지 <b>{props.diaryList.length}</b>개의 일기를 작성하셨네요!
+
+
+                    </div>
+                        <div className="d-grid gap-2">
+                            &nbsp;
+                            <Button
+                                variant="primary"
+                                style={{backgroundColor: "007AFF", fontWeight: "600"}}
+                                onClick={props.navigateToWriting}>
+                            📝 오늘의 일기 작성하러 가기
+                        </Button>
+
+                        <Button
+                            variant="dark"
+                            style={{backgroundColor: "6c757d", fontWeight: "600"}}
+                            onClick={props.navigateToReview}>
+                            📖 작성한 일기 다시보기
+                        </Button>
+                        </div>
+                            </span>
+
+
+                </div>
+                {/*<Row>
+                    <Col>
+                        <div className="d-grid gap-2">
+                            <Button
+                                variant="primary"
+                                style={{fontWeight: "600"}}
+                                onClick={props.navigateToWriting}
+                            >✏️ 일기 작성하기</Button>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="d-grid gap-2">
+                            <Button
+                                variant="primary"
+                                style={{fontWeight: "600"}}
+                                onClick={props.navigateToReview}
+                            >📖 일기 돌아보기</Button>
+                        </div>
+
+                    </Col>
+                </Row>*/}
+                <span className="center_temp">
+                                                &nbsp;
+
+                    <Row xs={1} md={2} className="g-4">
+
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={book_purple}/>
+                            <Card.Body>
+                                <Card.Title><b>일기쓰기와 정신건강</b></Card.Title>
+                                <Card.Text>
+                                    일기를 작성하는 것이 어떻게 정신건강에 도움이 될까요?
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={chat}/>
+                            <Card.Body>
+                                <Card.Title><b>누구와 말하는 건가요?</b></Card.Title>
+                                <Card.Text>
+                                    마음챙김 다이어리가 어떻게 동작 원리에 대해 알아봅니다.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={lock}/>
+                            <Card.Body>
+                                <Card.Title><b>개인정보는 어떻게 관리되나요?</b></Card.Title>
+                                <Card.Text>
+                                    나의 데이터는 어떻게 관리되는지 알아봅니다.</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={book_blue}/>
+                            <Card.Body>
+                                <Card.Title><b>어떻게 적는건가요?</b></Card.Title>
+                                <Card.Text>
+                                    정신건강에 도움이 되는 일상 기록이란?
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+                </span>
+            </Row>
+            <div className="footer"></div>
         </Container>
-    );
+    )
 }
 
 export default Home;
