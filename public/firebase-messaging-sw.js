343b9f4e32fefbfa45797a5ee3138d00cee86c15
noveler-study <103440394+noveler-study@users.noreply.github.com>
2024-10-21 17:01:42 +0900

    Updated React application with new changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyA5As5BKbHxAtQLSbpB6wme_2Z0V57LpPU",
