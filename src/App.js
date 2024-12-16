fcb52fa51635c58b573dd68e82638e10ca5f50e7
haejii <yanghagi@naver.com>
2024-12-17 07:14:08 +0900

    프론트 배

--- src/App.js
+++ src/App.js
@@ -6,26 +6,39 @@ import React from 'react';
 import useSize from './component/common/useSize';
 import { extendTheme, ChakraProvider, StyleFunctionProps } from "@chakra-ui/react";
 import {useState, useEffect} from "react";
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
+import { messaging } from './firebase-config';
 import Auth from './pages/Auth';
 import Cookies from 'universal-cookie';
-
+import {signOut} from 'firebase/auth';
+import {auth, db} from './firebase-config';
 
 import Writing from "./pages/Writing";
 import Loading from "./pages/Loading";
 import DiaryList from "./pages/DiaryList";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-
+import {doc, getDoc} from "firebase/firestore";
+import { getToken } from 'firebase/messaging';
 import TopNav from './component/common/TopNav';
-import BottomNav from './component/common/BottomNav';
-import DiaryMock from "./pages/Result"
+import Write2 from './pages/Writing2'
+import DiaryMock from './pages/Result'
 
-const cookies = new Cookies();
+import { getEmail, getUserId, setEmail } from './localstorage/user';
+
+
 
 function App() {
     const { height } = useSize();
-  
+
+    useEffect(() => {
+      setEmail('hj@naber.com')
+      console.log(getEmail())
+    },[])
+   
+
+   
     const styles = {
         global: (props: StyleFunctionProps) => ({
           html: {
@@ -63,19 +76,25 @@ function App() {
                     <Route path="/" element={
                         <div>
                             {/* 로그인 한 상태이면 HOME 으로 그렇지 않으면 Auth 로그인 페이지로 이동 */}
-                            {isAuth ? ( <><Home userName={userName} userMail={userMail} diaryCount={diaryCount}/><BottomNav number={1} /></>) : (
-                                <Auth setIsAuth={setIsAuth} setUserName={setUserName}/>)}
+                            <Auth/>
+                            {/* {isAuth ? ( <><Home userName={userName} userMail={userMail} diaryCount={diaryCount}/><BottomNav number={1} /></>) : (
+                                <Auth setIsAuth={setIsAuth} setUserName={setUserName}/>)} */}
                         </div>
                     }/>
-                    <Route path="/writing"
+                    <Route path="/home" element={<Home/>}/>
+                    {/* <Route path="/writing" element={<Writing userName={'Lamda'} userMail={'skku@gmail.com'}/>}/> */}
+                    <Route path="/writing2" element={<Write2 />}/>
+                    {/* <Route path="/writing"
                            element={isAuth ? (<div><Writing userName={userName} userMail={userMail}/><BottomNav number={2}/></div>) : (
                                <Auth setIsAuth={setIsAuth} setUserName={setUserName} setUserMail={setUserMail}/>)
                            }/>
                     <Route path="/list"
-                           element={isAuth ? (<div><DiaryMock /><BottomNav number={3}/></div>) : (
+                           element={isAuth ? (<div><DiaryList userName={userName} userMail={userMail} number={3} /><BottomNav number={3}/></div>) : (
                                <Auth setIsAuth={setIsAuth} setUserName={setUserName} setUserMail={setUserMail}/>)}/>
-                    <Route path="/loading" element={<div><Loading/></div>}/>
-                    <Route path="*" element={<div>404~ 없는페이지임</div>}/>
+                    <Route path="/loading" element={<div><Loading/></div>}/> */}
+                    <Route path='/list' element={<DiaryMock/>}/>
+                   <Route path="*" element={<div>404~ 없는페이지임</div>}/>
+                    <Route path="/signup" element={<Signup/>}/>
                 </Routes>
             </div>
 
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,117 +1,97 @@
-import {auth, provider} from "../firebase-config"
-import {signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { auth, provider } from "../firebase-config";
+import {
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 
-import Cookies from 'universal-cookie'
-
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 import { Flex, Text, Box, Input, Button } from "@chakra-ui/react";
 
-
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { ColorSigniture } from "../utils/_Palette";
 
-const cookies = new Cookies()
-
-export const Auth = (props) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordCheck, setPasswordCheck] = useState("");
-    const [username, setUsername] = useState("");
-    const [newUser, setNewUser] = useState(false);
-
-    const signInWithGoogle = async () => {
-        try {
-            const result = await signInWithPopup(auth, provider);
-            cookies.set("auth-token", result.user.refreshToken);
-            props.setIsAuth(true)
-            props.setUserName(auth.currentUser.displayName)
-        } catch (err) {
-            console.error(err);
-
-        }
-    };
-
-    const signInWithEmailPassword = async () => {
-        try {
-            const result = await signInWithEmailAndPassword(auth, email, password);
-            cookies.set("auth-token", result.user.refreshToken);
-            props.setIsAuth(true)
-            props.setUserName(auth.currentUser.displayName)
-        } catch (err) {
-            console.error(err);
-            if (err.message.includes("wrong-password")) {
-                alert("비밀번호가 틀렸습니다. 비밀번호가 기억나지 않으신다면, taewan@kaist.ac.kr 또는 010-9085-2356으로 연락부탁드립니다.");
-            } else if (err.message.includes("user-not-found")) {
-                alert("계정정보가 없습니다.")
-            } else if (err.message.includes("invalid-email")) {
-                alert("올바른 이메일 형식이 아닙니다.")
-            } else {
-                alert('Error: ' + err.message);
-            }
-        }
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const signInWithEmailPassword = async () => {
+    try {
+      //   const result = await signInWithEmailAndPassword(auth, email, password);
+      //   cookies.set("auth-token", result.user.refreshToken);
+      //   props.setIsAuth(true);
+      //   props.setUserName(auth.currentUser.displayName);
+      //회원가입 성공 시 home 페이지로 빼주고 Localstorage에 저장
+    } catch (err) {
+      console.error(err);
+      if (err.message.includes("wrong-password")) {
+        alert(
+          "비밀번호가 틀렸습니다. 비밀번호가 기억나지 않으신다면, taewan@kaist.ac.kr 또는 010-9085-2356으로 연락부탁드립니다."
+        );
+      } else if (err.message.includes("user-not-found")) {
+        alert("계정정보가 없습니다.");
+      } else if (err.message.includes("invalid-email")) {
+        alert("올바른 이메일 형식이 아닙니다.");
+      } else {
+        alert("Error: " + err.message);
+      }
     }
+  };
 
-    const triggerCall = () => {
-        if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.callKitHandler) {
-            console.log("Native bridge available, sending message to iOS");
-            window.webkit.messageHandlers.callKitHandler.postMessage("triggerCall");
-        } else {
-            console.warn("Native bridge not available.");
-        }
-    };
-    
+  const goToSignUp = () => {
+    navigate("/signup");
+  };
 
-
-    const signUpWithEmailPassword = async () => {
-        if (password !== passwordCheck) {
-            alert("비밀번호가 일치하지 않습니다. 동일한 비밀번호를 입력해주세요");
-        } else {
-            try {
-                const result = await createUserWithEmailAndPassword(auth, email, password);
-                await updateProfile(auth.currentUser, {
-                    displayName: username
-                });
-                cookies.set("auth-token", result.user.refreshToken);
-                props.setIsAuth(true)
-                props.setUserName(auth.currentUser.displayName)
-            } catch (err) {
-                console.error(err);
-                if (err.message.includes("email-already-in-use")) {
-                    alert("이미 가입된 이메일입니다. 비밀번호가 기억나지 않으신다면, taewan@kaist.ac.kr 또는 010-9085-2356으로 연락부탁드립니다.");
-                } else if (err.message.includes("invalid-email")) {
-                    alert("올바른 이메일 형식이 아닙니다.")
-                } else if (err.message.includes("weak-password")) {
-                    alert("비밀번호는 6자 이상으로 설정해주세요")
-                } else {
-                    alert('Error: ' + err.message);
-                }
-            }
-
-        }
-
-    }
-
-
-
-    return (
-        <Box minH={'calc(100vh - 130px)'} alignContent="center" mx='12px'>
-            <Flex flexDir={'column'} justifyContent={'center'} alignItems={'center'} mt='-100px'>
-            <Text fontSize={'20px'} fontWeight={400} mb='0px'>Meet your personal counselor through your own diary 🙂</Text>
-            <Text fontSize={'32px'} fontWeight={700}>Log In</Text>
-            <Input placeholder="✉️ Please enter your email address" type="email" onChange={(e) => setEmail(e.target.value)}  mb='12px'/>
-            <Input  placeholder="🔑 Please enter your password" type="password" onChange={(e) => setPassword(e.target.value)} mb='30px'/>
-            <Button   
-                backgroundColor={ColorSigniture}
-                color={'white'}
-                width={'100%'} 
-                borderRadius={'20px'}
-                onClick={signInWithEmailPassword}>
-           Log In with Email/Password</Button>
-            </Flex>
-            <Text mt='12px'>Having trouble logging in?</Text>
-      
-            <Button onClick={()=>triggerCall()}>전화 알림 테스트</Button>
-
-        </Box>
-    )
-}
+  return (
+    <Box minH={"calc(100vh - 130px)"} alignContent="center" mx="12px">
+      <Flex
+        flexDir={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        mt="-100px"
+      >
+        <Text fontSize={"20px"} fontWeight={400} mb="0px">
+          나만의 일기로 상담사를 만나요 🙂
+        </Text>
+        <Text fontSize={"32px"} fontWeight={700}>
+          로그인 하기
+        </Text>
+        <Input
+          placeholder="✉️ Please enter your email address"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          mb="12px"
+        />
+        <Input
+          placeholder="🔑 Please enter your password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          mb="30px"
+        />
+        <Button
+          backgroundColor={ColorSigniture}
+          color={"white"}
+          width={"100%"}
+          borderRadius={"20px"}
+          onClick={signInWithEmailPassword}
+        >
+          로그인 하기
+        </Button>
+        <Button
+          width={"100%"}
+          onClick={goToSignUp}
+          mt="10px"
+          borderRadius={"20px"}
+        >
+          회원가입
+        </Button>
+      </Flex>
+      <Text mt="12px">로그인에 문제가 있나요?</Text>
+    </Box>
+  );
+};
 export default Auth;
